cdd3359cf725fac2f827422c9e1bef5729e1b3ed
Fabio <sfabio.mail@gmail.com>
2025-04-22 14:06:43 +0200

    ci: try to fix hardcoded secret

--- Project/settings.py
+++ Project/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sdhaihdie987392bsjdbjs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -149,6 +148,6 @@ MEDIA_ROOT = os.path.join((BASE_DIR), "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL='/user/login/'
-LOGIN_REDIRECT_URL='/home/'
+LOGIN_URL = '/user/login/'
+LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/home/'
